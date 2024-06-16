5425397a2e79658752da82477ea741a64ac8adcf
Marko Krstic <mailme@markokrstic.com>
2024-06-16 08:53:46 +0200

    block

--- test.php
+++ test.php
@@ -18,8 +18,24 @@
 
 
  
-function myplugin_scripts() {
-    wp_register_style( 'foo-styles',  plugin_dir_url( __FILE__ ) . 'style.css' );
-    wp_enqueue_style( 'foo-styles' );
+// function myplugin_scripts() {
+//     wp_register_style( 'foo-styles',  plugin_dir_url( __FILE__ ) . 'style.css' );
+//     wp_enqueue_style( 'foo-styles' );
+// }
+// add_action( 'wp_enqueue_scripts', 'myplugin_scripts' );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
-add_action( 'wp_enqueue_scripts', 'myplugin_scripts' );
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_selfhost_block_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_selfhost_block_block_init' );
